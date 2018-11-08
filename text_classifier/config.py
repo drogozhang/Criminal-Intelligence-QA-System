dcf32be02e4ca616b2cb0977a99c7d3a3113fc8f
sunlin <sunl@zucc.edu.cn>
2018-11-09 04:50:06 +0800

    train samples in 18-11-9 argument class

---
+++ text_classifier/config.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+#
+# Created by Drogo Zhang
+#
+# On 2018-11-08
+
+
+WORD_VECTOR_MODEL_PATH = "C:\\Users\\DrogoKhal\\MachineLearning\\Paper\\baidu_encyclopedia\\baidu_encyclopedia"  # "all.seg300w50403.model"  # "C:\\Users\\DrogoKhal\\MachineLearning\\Paper\\baidu_encyclopedia\\baidu_encyclopedia"
+
+TRAIN_CSV_PATH = "11-9 advancedJavaTrainSamplesV1.csv"
+JIEBA_USERDICT_PATH = "judicial_userdict.txt"
+
+CLASSES_NUM = 15  # 11-8 classes num
+
+MAX_SENTENCE_LENGTH = 20
+WORD_EMBEDDING_DIM = 300
